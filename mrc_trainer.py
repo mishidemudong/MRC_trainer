d702ad75d50816a1b993e9c4c9e3bcd57619e3a2
mishidemudong <liang.dongkai@163.com>
2020-09-14 10:51:24 +0800

    add lcs func

--- mrc_trainer.py
+++ mrc_trainer.py
@@ -29,6 +29,18 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 sess = tf.Session(config=config)
 
+def lcsubstr(s1, s2):
+    m = [[0 for i in range(len(s2) + 1)] for j in range(len(s1) + 1)]  # 生成0矩阵，为方便后续计算，比字符串长度多了一列
+    mmax = 0  # 最长匹配的长度
+    p = 0  # 最长匹配对应在s1中的最后一位
+    for i in range(len(s1)):
+        for j in range(len(s2)):
+            if s1[i] == s2[j]: # 如果相等，则加入现有的公共子串
+                m[i + 1][j + 1] = m[i][j] + 1
+                if m[i + 1][j + 1] > mmax:
+                    mmax = m[i + 1][j + 1]
+                    p = i + 1
+    return s1[p - mmax:p], mmax  # 返回最长子串及其长度
 
 class data_generator(DataGenerator):
     """数据生成器
